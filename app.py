ad31f8c36f4838862c94ec1252b546ec29450ffb
iecoxe <i153140965@163.com>
2020-10-10 12:26:52 +0800

    append uwsgi server

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from flask_script import Manager
+
 from music import create_app
 
 app = create_app()
 
-
+# 初始化命令行模式
+manager = Manager(app)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
--- music/__init__.py
+++ music/__init__.py
@@ -17,8 +17,6 @@ def create_app():
     # 初始化第三方插件
     init_app(app)
 
-    # 初始化命令行模式
-    manager = Manager(app)
 
     # 最终上线需要 return app
-    return manager
+    return app
